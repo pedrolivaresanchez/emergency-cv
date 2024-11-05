3c734315bea3916379815af7d23c698e2f5f4e95
Pablo López <pablo@bold-factory.com>
2024-11-05 22:47:48 +0100

    refactor: use autogenerated typed client for supabase

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,6 +1,7 @@
 import { createBrowserClient } from '@supabase/ssr';
+import { Database } from '@/types/database';
 
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
 const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "";
 
-export const supabase = createBrowserClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createBrowserClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
