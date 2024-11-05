c889fb3fec981db5035362f772686de3de789cc5
Pablo Gracia Gil <pablo.graciag@gmail.com>
2024-11-05 17:07:46 +0100

    Separate renderer and container & add error message

--- src/components/PhoneInput/PhoneInputRenderer.tsx
+++ src/components/PhoneInput/PhoneInputRenderer.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-type PhoneInputProps = {
-  onChange: React.ChangeEventHandler<HTMLInputElement>;
-  phoneNumber: string;
-  required: boolean;
-};
+import { PhoneInputProps } from './types';
 
-export function PhoneInput({ onChange, phoneNumber, required = false }: PhoneInputProps) {
+export function PhoneInputRenderer({ onChange, phoneNumber, required = false }: PhoneInputProps) {
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    const cleanValue = e.target.value.replace(/[^0-9\s]/g, '');
+    onChange(cleanValue);
+  };
   return (
-    <div className="mb-4">
+    <>
       <label htmlFor="phone" className="block text-sm font-medium text-gray-700 mb-1">
         Número de teléfono {required ? <span className="text-red-500">*</span> : null}
       </label>
@@ -24,14 +24,15 @@ export function PhoneInput({ onChange, phoneNumber, required = false }: PhoneInp
             type="tel"
             id="phone"
             value={phoneNumber}
-            onChange={onChange}
+            onChange={handleChange}
             className="w-full bg-transparent px-3 py-2 text-gray-600 placeholder-gray-500 focus:outline-none text-sm"
             placeholder="612 345 678"
+            pattern="^[0-9\s]*$"
             maxLength={12} // Accounts for white spaces
             required
           />
         </div>
       </div>
-    </div>
+    </>
   );
 }
