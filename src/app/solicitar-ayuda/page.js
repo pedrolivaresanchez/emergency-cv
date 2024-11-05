c77e756c02059721829c17ab4ba635965ec183c1
Pablo Gracia Gil <pablo.graciag@gmail.com>
2024-11-05 12:44:00 +0100

    Cambiar el texto y anhadir links

--- src/app/solicitar-ayuda/page.js
+++ src/app/solicitar-ayuda/page.js
@@ -10,6 +10,7 @@ import { helpRequestService } from '@/lib/service';
 
 import { PhoneInput } from '@/components/PhoneInput';
 import { formatPhoneNumber } from '@/helpers/format';
+import Image from 'next/image';
 
 export default function SolicitarAyuda() {
   const [formData, setFormData] = useState({
@@ -160,15 +161,40 @@ export default function SolicitarAyuda() {
       <div className="bg-red-100 border-l-4 border-red-500 p-4 rounded">
         <div className="flex items-start">
           <AlertTriangle className="h-5 w-5 text-red-500 mt-0.5 mr-2" />
-          <div>
+          <div className="space-y-4">
             <h2 className="text-red-800 font-bold">PARA PERSONAS CON DIFICULTADES TECNICAS</h2>
-            <p className="text-red-700 text-sm mt-1">
-              Hemos habilitado el número{' '}
-              <a className="font-bold text-blue-600 hover:text-blue-800" href="tel:+34626675591">
-                626 675 591
-              </a>{' '}
-              para facilitar la petición de ayuda a aquellas personas que encuentren complicado usar la página web.
-            </p>
+            <div className="flex flex-col text-red-700 space-y-2">
+              <p className="font-bold">
+                AVISO IMPORTANTE: Esta información es sólo para personas que tengan dificultades técnicas a la hora de
+                pedir ayuda.
+              </p>
+              <p className="mb-2">
+                Hemos habilitado el número{' '}
+                <a className="font-bold text-blue-600 hover:text-blue-800" href="tel:+34626675591">
+                  626 675 591
+                </a>{' '}
+                para facilitar la petición de ayuda a aquellas personas que encuentren complicado usar la página web.{' '}
+              </p>
+              <p className="font-bold">
+                ¡Importante! No saturéis el teléfono si podéis usar la página web, por favor. Si tenéis alguna duda
+                sobre la página web o deseas aportar nuevas ideas, por favor escríbenos a{' '}
+                <a className="text-blue-600 hover:text-blue-800" href="mailto:info@ajudadana.es">
+                  info@ajudadana.es
+                </a>
+              </p>
+              <p>También puedes contactar con nosotros a través de:</p>
+              <a className="text-blue-600 hover:text-blue-800 flex space-x-2" href="https://wa.me/34626675591">
+                <Image
+                  src={
+                    'https://upload.wikimedia.org/wikipedia/commons/a/a7/2062095_application_chat_communication_logo_whatsapp_icon.svg'
+                  }
+                  alt="Whatsapp icon"
+                  height={20}
+                  width={20}
+                />
+                <span className="font-semibold">WhatsApp</span>
+              </a>
+            </div>
           </div>
         </div>
       </div>
