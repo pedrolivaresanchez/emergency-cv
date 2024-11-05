f6169dc738f7b82f09a17cc18e2c2f084161c0f9
Pablo Gracia Gil <pablo.graciag@gmail.com>
2024-11-05 17:30:24 +0100

    Remove unused function

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -2,5 +2,3 @@ export const isValidPhone = (phoneNumber) => {
   const phoneNumberWithoutSpaces = phoneNumber.replace(/\D/g, '');
   return phoneNumberWithoutSpaces.length === 9 && /^\d[ \d]*$/.test(phoneNumberWithoutSpaces);
 };
-
-export const isNumericOrSpaces = (value) => /^\d*\s*$/.test(value);
