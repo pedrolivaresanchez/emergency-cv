d7fff378823c9673e77133b9989a214c53609e95
Pinx0 <plponce@gmail.com>
2024-11-06 10:42:52 +0100

    refactor: add additional info type

--- src/types/Requests.ts
+++ src/types/Requests.ts
@@ -6,6 +6,11 @@ export type HelpRequestStatusType = 'pending' | 'in_progress' | 'active';
 export type HelpRequestData = Database['public']['Tables']['help_requests']['Row'];
 export type HelpRequestAssignmentData = Database['public']['Tables']['help_request_assignments']['Row'];
 
+export type HelpRequestAdditionalInfo = {
+  special_situations?: string;
+  email?: string;
+}
+
 export type CollectionPointType = 'permanente' | 'temporal';
 export type CollectionPointStatus = 'active' | 'inactive';
 export type CollectionPointData = Database['public']['Tables']['collection_points']['Row'];
