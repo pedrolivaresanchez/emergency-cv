0cfeb320c4ccea53e5b527cdab887a3e66ac1a48
Roberto Milla Martinez <roberto@accounton.io>
2024-11-06 19:14:03 +0100

    fix: normalize to existing fields in db

--- src/app/api/address/route.tsx
+++ src/app/api/address/route.tsx
@@ -1,8 +1,44 @@
 import { NextRequest } from 'next/server';
 
+/* 
+// Campos que pueden generar problema todavia -- [db: mapsResponse]
+
+// cual de los dos nos quedamos? los unificamos? Podemos deprecar Alcudia y ya
+- 'Alcudia': La Alcudia | L'Alcúdia  
+- 'L'Alcúdia': La Alcudia | L'Alcúdia 
+// Cambiar texto en la base de datos ?? 
+- 'Castelló': Castellón de la Plana | Castelló de la Plana 
+// eliminar de la base de datos?  
+- 'Manuel', ???? 
+
+
+// maps lo detecta como valencia
+- 'La Torre, Valencia': Valencia | València 
+// maps lo detecta como Valencia
+- 'Benimaclet': Valencia | València
+*/
+
+const mapsTranslationToDbTowns: { [key: string]: string } = {
+  Aldaya: 'Aldaia',
+  'Ribarroja de Turia': 'Riba-roja de Túria',
+  Benetuser: 'Benetusser',
+  Toris: 'Turís',
+  Picaña: 'Picanya',
+  'La Alcudia': "L'Alcúdia",
+  'Lugar Nuevo de la Corona': 'Llocnou de la Corona',
+};
+
 const GOOGLE_URL = `https://maps.googleapis.com/maps/api/geocode/json?key=${process.env.API_KEY}&latlng=`;
 
-const extractAddressAndTown = (googleResponse: any) => {
+export type AddressAndTown = { address: string; town: string };
+
+function normalizeData({ address, town }: AddressAndTown): AddressAndTown {
+  const normalizedTown = Object.keys(mapsTranslationToDbTowns).includes(town) ? mapsTranslationToDbTowns[town] : town;
+  const normalizedAddress = address.replace(town, normalizedTown);
+  return { address: normalizedAddress, town: normalizedTown };
+}
+
+function extractAddressAndTown(googleResponse: any) {
   // for response refer to documentation: https://developers.google.com/maps/documentation/geocoding/requests-reverse-geocoding
   // it returns many due to inaccuracies but they only differ from street number(normally) - we look for a good result - contains sublocality
   let town = '';
@@ -16,20 +52,20 @@ const extractAddressAndTown = (googleResponse: any) => {
         if (type === 'locality') {
           localityFound = true;
           town = addressComponent['long_name'];
-
           break;
         }
       }
 
       if (localityFound) {
         address = result['formatted_address'];
-        return { address, town };
+
+        return normalizeData({ address, town });
       }
     }
   }
 
   return { address, town };
-};
+}
 
 // we should protect this with only authenticated users!
 export async function POST(request: NextRequest) {
