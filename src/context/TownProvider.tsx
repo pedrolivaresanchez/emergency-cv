195085f6f409a8a72e0cdee0f0d58adb2bf05b3c
Pinx0 <plponce@gmail.com>
2024-11-06 10:33:28 +0100

    refactor: add town type

---
+++ src/context/TownProvider.tsx
@@ -0,0 +1,16 @@
+'use client';
+import React, { createContext, ReactNode, useContext } from 'react';
+import { Town } from '@/types/Town';
+
+const TownsContext = createContext<Town[]>([]);
+
+type TownsProviderProps = {
+  children: ReactNode;
+  towns: Town[];
+}
+
+export const TownsProvider: React.FC<TownsProviderProps> = ({ children, towns }) => {
+  return <TownsContext.Provider value={towns}>{children}</TownsContext.Provider>;
+};
+
+export const useTowns = () => useContext(TownsContext);
