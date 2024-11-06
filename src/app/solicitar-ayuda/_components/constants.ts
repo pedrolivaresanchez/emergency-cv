a1b4709edf4e8f5e89d7dcf46b153dc254cc43f2
Pablo Gracia Gil <pablo.graciag@gmail.com>
2024-11-06 13:23:58 +0100

    Add new help types

---
+++ src/app/solicitar-ayuda/_components/constants.ts
@@ -0,0 +1,43 @@
+import { HelpCategory } from './types';
+
+export const TIPOS_DE_AYUDA: HelpCategory[] = [
+  {
+    id: 1,
+    label: 'Labores de limpieza',
+  },
+  {
+    id: 2,
+    label: 'Petición de desplazamiento',
+  },
+  {
+    id: 3,
+    label: 'Alojamiento temporal',
+  },
+  {
+    id: 4,
+    label: 'Necesidad de suministros y material',
+  },
+  {
+    id: 5,
+    label: 'Equipo de rescate (evacuación)',
+  },
+  {
+    id: 6,
+    label: 'Asistencia médica',
+  },
+  {
+    id: 7,
+    label: 'Apoyo psicológico',
+  },
+  {
+    id: 8,
+    label: 'Apoyo logístico',
+  },
+  { id: 9, label: 'Maquinaria para movilidad reducida' },
+  { id: 10, label: 'Maquinaria pesada (grúas, palas, ...)' },
+  {
+    id: 11,
+    label: 'Contenedores de escombros',
+  },
+  { id: 12, label: 'Equipamiento (botas, palas, guantes, ...)' },
+];
