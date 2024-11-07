81ee1be4e442413d07b61cd80054ef33bf6d5af9
Troleomotor10 <frack.7000@gmail.com>
2024-11-07 23:02:09 +0100

    fix: google profile image

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,14 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
