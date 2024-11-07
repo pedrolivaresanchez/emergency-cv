9367aad4200c639a91bdff743673b6f68ebb75fe
Liinares <miguellinares908@gmail.com>
2024-11-07 17:40:48 +0100

    fix: update user metadata access in FormContainer

--- src/app/solicitar-ayuda/_components/Form/FormContainer.tsx
+++ src/app/solicitar-ayuda/_components/Form/FormContainer.tsx
@@ -32,8 +32,10 @@ export function FormContainer() {
   const router = useRouter();
   const session = useSession();
 
+  console.log('SESION', session?.user?.user_metadata);
+
   const [formData, setFormData] = useState<FormData>({
-    nombre: session?.user?.user_metadata?.full_name.split(" ")[0]|| '',
+    nombre: session?.user?.user_metadata?.nombre?.split(' ')[0] || '',
     ubicacion: '',
     coordinates: null,
     tiposDeAyuda: new Map(TIPOS_DE_AYUDA.map(({ id }) => [id, false])),
@@ -91,7 +93,7 @@ export function FormContainer() {
       try {
         const helpRequestData: Database['public']['Tables']['help_requests']['Insert'] = {
           type: 'necesita',
-          name: formData.nombre.split(" ")[0],
+          name: formData.nombre.split(' ')[0],
           location: formData.ubicacion,
           latitude: formData.coordinates ? parseFloat(formData.coordinates.lat) : null,
           longitude: formData.coordinates ? parseFloat(formData.coordinates.lng) : null,
