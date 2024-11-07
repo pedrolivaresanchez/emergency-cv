fa734cf242908fbaef447b3745a652099e261c33
Pinx0 <plponce@gmail.com>
2024-11-07 20:00:50 +0100

    style: run prettier

--- src/app/auth/page.js
+++ src/app/auth/page.js
@@ -6,11 +6,11 @@ import { authService } from '@/lib/service';
 
 export default function AuthPage() {
   const router = useRouter();
-  
+
   useEffect(() => {
     async function fetchSession() {
       const { data: session } = await authService.getSessionUser();
-      if (session.user) {       
+      if (session.user) {
         router.push('/');
       }
     }
