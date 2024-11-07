58eef5b0e7727aa04b3634339624dc7549c35d36
Pinx0 <plponce@gmail.com>
2024-11-07 21:10:23 +0100

    refactor: simplify logic to query a request

---
+++ src/app/solicitudes/editar/[id]/page.tsx
@@ -0,0 +1,25 @@
+import RequestHelp from '@/components/RequestHelp';
+import Unauthorized from '@/components/Unauthorized';
+import { helpRequestService } from '@/lib/service';
+import { createClient } from '@/lib/supabase/server';
+
+export default async function EditarSolicitud({ params }: { params: { id: string } }) {
+  const { id } = params;
+  const supabase = await createClient();
+  const { data: session } = await supabase.auth.getUser();
+  if (session.user === null) {
+    return <Unauthorized />;
+  }
+  const request = await helpRequestService.getOne(Number(id));
+
+  return (
+    <RequestHelp
+      title="Editar solicitud de ayuda"
+      submitType="edit"
+      data={request}
+      id={id}
+      button={['Editar solicitud', 'Editando solicitud...']}
+      redirect="/solicitudes"
+    />
+  );
+}
