c92b62a8036fd5a46665d74c25d3d42b0a2f9e9d
patrickwebsdev <contacto@patricioalmada.com.ar>
2024-11-07 22:03:26 -0300

    fix: supabase update

--- supabase/migrations/20241108005004_add_help_requests_policy_update_with_email.sql
+++ supabase/migrations/20241108005004_add_help_requests_policy_update_with_email.sql
@@ -1,8 +1,6 @@
-alter policy "Enable update for users based on email"
-on "public"."help_requests"
+alter policy "Enable_update_for_users_based_on_email"
+on public.help_requests
 to public
 using (
 	  ((auth.uid() IS NOT NULL) AND ((additional_info ->> 'email'::text) = auth.email()))
-with check (
-	((auth.uid() IS NOT NULL) AND ((additional_info ->> 'email'::text) = auth.email()))
-);
+)
