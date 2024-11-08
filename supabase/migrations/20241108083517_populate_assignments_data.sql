04f2b0a4534da850aba37f8af6078012205847fc
Pablo Gracia Gil <pablo.graciag@gmail.com>
2024-11-08 08:38:54 +0100

    typo

--- supabase/migrations/20241108083517_populate_assignments_data.sql
+++ supabase/migrations/20241108083517_populate_assignments_data.sql
@@ -1,5 +1,5 @@
 UPDATE "public"."help_requests"
-SET "assignees_count" = (
+SET "asignees_count" = (
     SELECT COUNT(*)
     FROM "public"."help_request_assignments"
     WHERE "public"."help_request_assignments"."help_request_id" = "public"."help_requests"."id"
