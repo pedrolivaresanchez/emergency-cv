99fcb3283c4ea95d45a6ab93bc7041a5149047e1
patrickwebsdev <contacto@patricioalmada.com.ar>
2024-11-08 12:10:44 -0300

    fix: types

--- src/types/Requests.ts
+++ src/types/Requests.ts
@@ -3,7 +3,7 @@ import { Database } from './database';
 export type HelpRequestType = 'necesita' | 'ofrece';
 export type HelpRequestUrgencyType = 'alta' | 'media' | 'baja';
 export type HelpRequestStatusType = 'pending' | 'in_progress' | 'active';
-export type HelpRequestData = Database['public']['Tables']['help_requests']['Row'];
+export type HelpRequestData = Database['public']['Tables']['help_requests']['Row'] & { special_situations:string, user_id: string};
 export type HelpRequestUpdate = Database['public']['Tables']['help_requests']['Update'];
 export type HelpRequestAssignmentData = Database['public']['Tables']['help_request_assignments']['Row'];
 export type HelpRequestAssignmentInsert = Database['public']['Tables']['help_request_assignments']['Insert'];
@@ -12,3 +12,7 @@ export type HelpRequestAdditionalInfo = {
   special_situations?: string;
   email?: string;
 };
+
+export type CollectionPointType = 'permanente' | 'temporal';
+export type CollectionPointStatus = 'active' | 'inactive';
+export type CollectionPointData = Database['public']['Tables']['collection_points']['Row'];
