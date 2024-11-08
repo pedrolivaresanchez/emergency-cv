4c738b4f2188209c4d6a0b45c4c7a81765969a9a
Pinx0 <plponce@gmail.com>
2024-11-08 02:52:49 +0100

    fix: update session after each change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import './globals.css';
 import 'leaflet/dist/leaflet.css';
 import SidebarLayout from '@/components/layout/SidebarLayout';
 import { ModalProvider } from '@/context/ModalProvider';
-import { createClient } from '@/lib/supabase/server';
 import { SessionProvider } from '@/context/SessionProvider';
 import { Toaster } from 'sonner';
 import { PropsWithChildren } from 'react';
@@ -13,19 +12,12 @@ export const metadata = {
   description: 'Sistema de coordinación para emergencias en la Comunidad Valenciana',
 };
 
-const getSession = async () => {
-  const supabase = await createClient();
-  const { data, error } = await supabase.auth.getUser();
-  return data;
-};
-
 export default async function RootLayout({ children }: PropsWithChildren) {
-  const session = await getSession();
   return (
     <html lang="es">
       <body suppressHydrationWarning={true}>
         <Toaster position="bottom-left" richColors />
-        <SessionProvider session={session}>
+        <SessionProvider>
           <QueryClientProvider>
             <ModalProvider>
               <SidebarLayout>{children}</SidebarLayout>
