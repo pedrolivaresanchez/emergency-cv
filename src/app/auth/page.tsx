9418f0150ddfab25fc4dfe8f7c48131889ae04fb
patrickwebsdev <contacto@patricioalmada.com.ar>
2024-11-08 03:19:42 -0300

    fix: on click sign in button - you redirect to home page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,17 +1,18 @@
 'use client';
 import { useEffect } from 'react';
 import Login from '../../components/auth/Login';
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { authService } from '@/lib/service';
 
 export default function AuthPage() {
   const router = useRouter();
-
+  const searchParams = useSearchParams();
+  const redirect = searchParams.get('redirect') || '/';
   useEffect(() => {
     async function fetchSession() {
       const { data: session } = await authService.getSessionUser();
       if (session.user) {
-        router.push('/');
+        router.push(redirect);
       }
     }
     fetchSession();
@@ -19,7 +20,7 @@ export default function AuthPage() {
 
   return (
     <section className="mx-6 lg:m-16">
-      <Login onSuccessCallback={() => (window.location.href = '/')} />
+      <Login onSuccessCallback={() => router.push(redirect)} redirectUrl={redirect} />
     </section>
   );
 }
