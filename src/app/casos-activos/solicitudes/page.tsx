61fc3579cfc2f8b72995804719242574fa531b65
Pinx0 <plponce@gmail.com>
2024-11-09 22:11:02 +0100

    fix: hide non-working toggle

--- src/app/casos-activos/solicitudes/page.tsx
+++ src/app/casos-activos/solicitudes/page.tsx
@@ -190,13 +190,13 @@ function Solicitudes() {
               ))}
             </select>
           </div>
-          <div className="flex flex-row flex-1 justify-end">
+          {/*<div className="flex flex-row flex-1 justify-end">
             <Toggle
               handleChange={handleToggleChange}
               checked={isStringTrue(filtroData.soloSinAsignar)}
               label="Sólo ofertas sin voluntarios"
             />
-          </div>
+          </div> */}
         </div>
       </div>
       <div className="grid gap-4">
